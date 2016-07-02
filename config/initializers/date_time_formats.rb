62fe13d29b20a267d7a27a565ac06fa90a6ef7d2
kruczjak <kruczjak@gmail.com>
2016-07-02 20:15:30 +0200

    Change time to 24h

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -6,4 +6,4 @@ Date::DATE_FORMATS[:medium] = '%b %-d, %Y'
 # :short  - 18 Jan 06:10
 # :medium - Jan 18, 2007 6:10am
 # :long   - January 18, 2007 06:10
-Time::DATE_FORMATS[:medium] = '%b %-d, %Y %-I:%M%P'
+Time::DATE_FORMATS[:medium] = '%b %-d, %Y %k:%M'
